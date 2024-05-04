4cb91e0c855c0c4ca9d5ec575b277ae23428a677
zvdy <cristian.030902@gmail.com>
2024-05-04 15:23:32 +0200

    [fix] Added graceful shutdown to coupon service and removed condition

--- coupon_service/cmd/coupon_service/main.go
+++ coupon_service/cmd/coupon_service/main.go
@@ -6,8 +6,9 @@ import (
 	"coupon_service/internal/repository/memdb"
 	"coupon_service/internal/service"
 	"fmt"
-	"runtime"
-	"time"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var (
@@ -15,27 +16,28 @@ var (
 	repo = memdb.New()
 )
 
-// Removed Yoda condition and added print for easy debugging.
-func init() {
-	numCPU := runtime.NumCPU()
-	fmt.Printf("Number of CPUs: %d\n", numCPU)
-	if numCPU == 32 { // You can modify this in order to test on non 32 core machines
-		fmt.Println("Panicking because this api NOTis meant to be run on 32 core machines")
-		panic("this api is NOT meant to be run on 32 core machines")
-	}
-}
-
-// Replaced tree with api for better readability and retrocompatibility with other IDE's.
-// Made it more readible.
 func main() {
 	serviceInstance := service.New(repo)
 	apiInstance := api.New(cfg.API, serviceInstance)
-	defer apiInstance.Close()
 
-	apiInstance.Start()
-	fmt.Println("Starting Coupon service server")
+	go func() {
+		apiInstance.Start()
+	}()
 
-	time.Sleep(1 * time.Hour * 24 * 365)
-	// Consider using events to handle graceful shutdown and easy handling/logging
-	fmt.Println("Coupon service server alive for a year, closing")
+	fmt.Printf("🚀 Starting Coupon service server on %v. Press Ctrl + C to stop the server.\n", cfg)
+
+	// Create a channel to receive OS signals
+	sigChan := make(chan os.Signal, 1)
+	// Notify sigChan when receiving SIGINT or SIGTERM
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until a signal is received
+	<-sigChan
+
+	fmt.Println("🛑 Shutting down the server...")
+
+	// Call the Close method of the apiInstance
+	apiInstance.Close()
+
+	fmt.Println("👋 Coupon service server stopped")
 }
